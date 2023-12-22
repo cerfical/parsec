c2019939ce2816e108511695869285d23d58abc9
cerfical <138404639+cerfical@users.noreply.github.com>
2023-12-22 22:13:53 +0500

    Add IndexRange to make referencing something by their index easier

---
+++ include/parsec/core/IndexRange.hpp
@@ -0,0 +1,44 @@
+#ifndef PARSEC_CORE_INDEX_RANGE_HEADER
+#define PARSEC_CORE_INDEX_RANGE_HEADER
+
+#include <gsl/util>
+
+namespace parsec {
+
+	/**
+	 * @brief Specifies a range of elements referenced by a range of indices.
+	*/
+	struct IndexRange {
+
+		IndexRange(gsl::index start, gsl::index end) noexcept
+			: start(start), end(end)
+		{ }
+
+
+
+		/**
+		 * @brief Create a range containing one element.
+		*/
+		IndexRange(gsl::index start) noexcept
+			: IndexRange(start, start + 1)
+		{ }
+
+
+
+		/**
+		 * @brief Lower bound of the range.
+		*/
+		gsl::index start;
+
+
+
+		/**
+		 * @brief Upper bound of the range.
+		*/
+		gsl::index end;
+
+	};
+
+}
+
+#endif
