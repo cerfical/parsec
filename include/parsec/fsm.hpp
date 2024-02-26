3943e4edfcc2e1219d5a66263c8b270639f4b0bd
cerfical <138404639+cerfical@users.noreply.github.com>
2024-02-26 23:21:18 +0500

    Add fsm module to further replace dfa/lr modules

---
+++ include/parsec/fsm.hpp
@@ -0,0 +1,10 @@
+#ifndef PARSEC_FSM_ROOT_HEADER
+#define PARSEC_FSM_ROOT_HEADER
+
+/**
+ * @brief Provides convenient and simple-to-use utilities to represent and construct various kinds of FSMs.
+*/
+namespace parsec::fsm {
+}
+
+#endif
