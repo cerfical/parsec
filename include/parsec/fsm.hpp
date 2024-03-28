b92d5086d0ed26f518c27a834651643f0e762796
cerfical <138404639+cerfical@users.noreply.github.com>
2024-03-28 15:55:22 +0500

    Remove deprecated fsm module

--- include/parsec/fsm.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef PARSEC_FSM_ROOT_HEADER
-#define PARSEC_FSM_ROOT_HEADER
-
-#include "fsm/State.hpp"
-#include "fsm/StateMachine.hpp"
-#include "fsm/Factory.hpp"
-#include "fsm/print.hpp"
-
-#endif
