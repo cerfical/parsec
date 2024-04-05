486d1ca7b04f3c30a2ba2236641464ad82722cd8
cerfical <138404639+cerfical@users.noreply.github.com>
2024-04-05 21:44:04 +0500

    Fix misplaced visual markers in error messages

--- app/main.cpp
+++ app/main.cpp
@@ -4,23 +4,21 @@
 #include <boost/algorithm/string/trim.hpp>
 #include <boost/program_options.hpp>
 
-#include <gsl/gsl>
-
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <filesystem>
 #include <ranges>
 
 namespace fs = std::filesystem;
+namespace views = std::views;
 namespace algo = boost::algorithm;
 namespace po = boost::program_options;
 
-namespace ranges = std::ranges;
-namespace views = std::views;
-
 class ParsecApp {
 public:
-	int exec(int argc, gsl::czstring argv[]) noexcept {
+
+	int exec(int argc, const char* argv[]) noexcept {
 		int exitCode = 0;
 		try {
 			if(parseCommandLine(argc, argv)) {
@@ -34,11 +32,11 @@ public:
 	}
 
 private:
-	/** @{ */
-	bool parseCommandLine(int argc, gsl::czstring argv[]) {
+	
+	bool parseCommandLine(int argc, const char* argv[]) {
 		po::options_description named = { "Options" };
 		named.add_options()
-			("input-file,i", po::value<fs::path>(), "input grammar file")
+			("input-file,i", po::value<fs::path>(), "input spec file")
 			("output-file,o", po::value<fs::path>(), "output source code file")
 			("version", "print version information")
 			("help", "produce help message");
@@ -89,6 +87,26 @@ private:
 		return true;
 	}
 
+	void compileGrammar() {
+		if(m_input.open(m_inputPath, std::ios::binary); !m_input.is_open()) {
+			throw std::runtime_error(std::format("failed to load the input file \"{}\"", m_inputPath.generic_string()));
+		}
+		m_input.exceptions(std::ios::badbit | std::ios::failbit);
+
+		// perform the compilation and save result in memory
+		std::ostringstream outputMem;
+		parsec::Compiler(outputMem)
+			.compile(m_input);
+
+		// if everything is fine, try to store the result in the actual output file
+		std::ofstream outputFile(m_outputPath);
+		if(!outputFile.is_open()) {
+			throw std::runtime_error(std::format("failed to load the output file \"{}\"", m_outputPath.generic_string()));
+		}
+		outputFile << std::move(outputMem).str();
+	}
+
+
 	void dumpCurrentException() {
 		try {
 			throw;
@@ -101,97 +119,69 @@ private:
 		}
 	}
 	
-	void compileGrammar() {
-		if(m_input.open(m_inputPath, std::ios::binary); !m_input.is_open()) {
-			throw std::runtime_error(std::format("failed to load the input file '{}'",
-				m_inputPath.generic_string()
-			));
-		}
-		m_input.exceptions(std::ios::badbit | std::ios::failbit);
-
-		if(m_output.open(m_outputPath); !m_output.is_open()) {
-			throw std::runtime_error(std::format("failed to load the output file '{}'",
-				m_outputPath.generic_string()
-			));
-		}
-
-		parsec::Compiler(m_output)
-			.compile(m_input);
-	}
-
-	void printLoc(std::ostream& out, const parsec::SourceLoc& loc) {
-		out << loc.lineNo() + 1 << ':' << loc.startCol() + 1;
-		if(loc.colCount()) {
-			out << '-' << (loc.endCol() - 1) + 1;
-		}
-	}
-
 	void printParseError(const parsec::ParseError& e) {
-		const auto line = readInputLineAt(e.loc().linePos());
-		const auto marker = makeVisualMarker(line, e.loc().startCol(), e.loc().colCount());
+		auto line = readInputLine(e.loc().linePos());
+		
+		const auto srcLineSize = line.size();
+		algo::trim_left(line);
+		const auto leadingWsSize = srcLineSize - line.size();
+		algo::trim_right(line);
+
+		const auto marker = makeVisualMarker(line, e.loc().startCol() - leadingWsSize, e.loc().colCount());
 		const auto indent = std::string(tabSize, ' ');
 
-		std::cerr << m_inputPath.generic_string() << ':';
-		printLoc(std::cerr, e.loc());
-		std::cerr << ": error: " << e.what() << '\n'
-			<< indent << algo::trim_copy(line) << '\n'
+		std::cerr
+			<< m_inputPath.generic_string() << ':' << e.loc() << ": error: " << e.what() << '\n'
+			<< indent << line << '\n'
 			<< indent << marker << '\n';
 	}
-	/** @} */
+	
 
-
-	/** @{ */
-	std::string makeVisualMarker(const std::string& line, int startCol, int colCount) {
-		const auto leadingWhitespace = line.size() - algo::trim_left_copy(line).size();
-		const auto marker = std::string(colCount, colCount == 1 ? '^' : '~');
-
-		std::string spaces;
-		for(const auto i : views::iota(0) | views::take(startCol - leadingWhitespace)) {
-			// tabs need to be treated specially as they can occur in the middle of a line
-			// and cause the visual marker to be placed in the wrong location
-			if(line[i] == '\t') {
-				// insert the required number of spaces to move to the next tab stop
-				const auto tabDisplaySize = tabSize - i % tabSize;
-				spaces.append(tabDisplaySize, ' ');
-			} else {
-				spaces.append(1, ' ');
-			}
-		}
-
-		return spaces + marker;
-	}
-
-	std::string readInputLineAt(int linePos) {
+	std::string readInputLine(int linePos) {
 		const auto inputPos = m_input.tellg();
 		m_input.seekg(linePos);
 
 		std::string line;
-		if(!checkForInputEof()) {
+		if(!isInputEnd()) {
 			std::getline(m_input, line);
 			m_input.seekg(inputPos);
 		}
 		return line;
 	}
 
-	bool checkForInputEof() {
+	bool isInputEnd() {
 		if(m_input.peek() == std::char_traits<char>::eof()) {
 			m_input.clear(m_input.rdstate() ^ std::ios::eofbit);
 			return true;
 		}
 		return false;
 	}
-	/** @} */
 
+
+	static std::string makeVisualMarker(const std::string& line, std::size_t pos, std::size_t size) {
+		std::size_t spaces = 0;
+		for(const auto& ch : line | views::take(pos)) {
+			// tabs need to be treated specially as they can occur in the middle of a line
+			// and cause the visual marker to be placed in the wrong location
+			if(ch == '\t') {
+				// insert the required number of spaces to move to the next tab stop
+				const auto tabDisplaySize = tabSize - spaces % tabSize;
+				spaces += tabDisplaySize;
+			} else {
+				spaces++;
+			}
+		}
+		return std::string(spaces, ' ') + std::string(size, size == 1 ? '^' : '~');
+	}
 
 	static constexpr int tabSize = 8;
 
-	std::ifstream m_input;
-	std::ofstream m_output;
 
+	std::ifstream m_input;
 	fs::path m_outputPath;
 	fs::path m_inputPath;
 };
 
-int main(int argc, gsl::czstring argv[]) noexcept {
+int main(int argc, const char* argv[]) noexcept {
 	return ParsecApp().exec(argc, argv);
 }
