1798914ca0e909aeb9c34200cbfc7ad29a96b15d
cerfical <138404639+cerfical@users.noreply.github.com>
2024-05-02 12:04:31 +0500

    Make Symbol instances shared and immutable to enable cheap copy operations

--- include/parsec/core/Symbol.hpp
+++ include/parsec/core/Symbol.hpp
@@ -3,49 +3,68 @@
 
 #include <string>
 #include <string_view>
+#include <memory>
+#include <compare>
 
 namespace parsec {
 
 	class Symbol {
 	public:
 
-		friend auto operator<=>(const Symbol&, const Symbol&) = default;
+		Symbol(char value) : Symbol({}, 1, value) {}
+
+		Symbol(const char* value) : Symbol({}, value) {}
+
+		Symbol(std::string_view value) : Symbol({}, value) {}
+
+		Symbol(const std::string& value) : Symbol({}, value) {}
+
+		Symbol(std::string&& value) : Symbol({}, std::move(value)) {}
+
+		Symbol() : Symbol({}, "") {}
 
 
-		Symbol(char value)
-			: m_value(1, value) {}
-
-		Symbol(const char* value)
-			: m_value(value) {}
-
-		Symbol(std::string_view value)
-			: m_value(value) {}
-
-		Symbol(const std::string& value)
-			: m_value(value) {}
-
-		Symbol(std::string&& value)
-			: m_value(std::move(value)) {}
-
-		Symbol() = default;
+		Symbol(const Symbol& other) noexcept = default;
+		Symbol& operator=(const Symbol& other) noexcept = default;
 
 
-		explicit operator bool() const {
+		Symbol(Symbol&& other) noexcept = default;
+		Symbol& operator=(Symbol&& other) noexcept = default;
+
+
+		const std::string& value() const noexcept {
+			return *m_value;
+		}
+
+
+		explicit operator bool() const noexcept {
 			return !isEmpty();
 		}
 
-		bool isEmpty() const {
-			return value().empty();
+
+		bool isEmpty() const noexcept {
+			return m_value->empty();
 		}
 		
-		const std::string& value() const {
-			return m_value;
-		}
-
 
 	private:
-		std::string m_value;
+		template <typename... Args>
+		explicit Symbol(int, Args&&... args)
+			: m_value(std::make_shared<std::string>(std::forward<Args>(args)...)) {}
+
+		std::shared_ptr<std::string> m_value;
 	};
+
+
+
+	inline bool operator==(const Symbol& lhs, const Symbol& rhs) noexcept {
+		return lhs.value() == rhs.value();
+	}
+
+
+	inline std::strong_ordering operator<=>(const Symbol& lhs, const Symbol& rhs) noexcept {
+		return lhs.value() <=> rhs.value();
+	}
 
 }
 
