9b15b3d42ef891f4447641267d8aab267fbec1d1
cerfical <138404639+cerfical@users.noreply.github.com>
2024-10-02 00:50:30 +0500

    Refactor main application code

--- app/main.cpp
+++ app/main.cpp
@@ -7,165 +7,190 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <ranges>
 #include <sstream>
 
 namespace fs = std::filesystem;
-namespace views = std::views;
 namespace algo = boost::algorithm;
 namespace po = boost::program_options;
-class ParsecApp {
+
+class ParsecOptions {
 public:
 
-    int exec(int argc, const char* argv[]) noexcept {
-        int exitCode = 0;
-        try {
-            if(parseCommandLine(argc, argv)) {
-                compileGrammar();
-            }
-        } catch(...) {
-            exitCode = 1;
-            dumpCurrentException();
-        }
-        return exitCode;
-    }
+    ParsecOptions(int argc, const char* argv[])
+        : argc_(argc), argv_(argv), named_("Options") {
+        named_.add_options()                                                                   //
+            ("input-file,i", po::value<fs::path>()->required(), "input spec file")             //
+            ("output-file,o", po::value<fs::path>(), "output source file")                     //
+            ("template,t", po::value<std::string>()->default_value("json"), "output template") //
+            ("template-dir", po::value<fs::path>(), "template directory")                      //
+            ("version", "print version information")                                           //
+            ("help", "produce help message");                                                  //
 
-private:
-
-    bool parseCommandLine(int argc, const char* argv[]) {
-        po::options_description named = { "Options" };
-        named.add_options()                                                                                    //
-            ("input-file,i", po::value<fs::path>(), "input spec file")                                         //
-            ("output-file,o", po::value<fs::path>(), "output source code file")                                //
-            ("template,t", po::value<std::string>()->default_value("json"), "template for output source code") //
-            ("template-path", po::value<fs::path>(), "template directory")                                     //
-            ("version", "print version information")                                                           //
-            ("help", "produce help message");                                                                  //
-
-        po::variables_map options;
         store(
             po::command_line_parser(argc, argv)
                 .positional(po::positional_options_description()
                                 .add("input-file", 1)
                                 .add("output-file", 1))
-                .options(named)
+                .options(named_)
                 .run(),
-            options
+            options_
         );
+    }
 
 
-        const bool helpRequested = options.contains("help") || options.size() == 1;
-        const bool versionRequested = options.contains("version");
-
-        if(helpRequested || versionRequested) {
-            if(options.size() != 2) {
+    bool dumpUsageInfo() {
+        if(options_.contains("help") || options_.contains("version")) {
+            if(argc_ != 2) {
                 throw std::runtime_error("invalid command line options");
             }
 
-            if(helpRequested) {
-                std::cout
-                    << "Usage:" << '\n'
-                    << "  parsec <input-file> [<output-file>]" << '\n'
-                    << "  parsec [options]" << '\n'
-                    << '\n'
-                    << named << '\n';
-            } else if(versionRequested) {
-                std::cout << std::format(
-                    "parsec version {}: {}",
+            if(options_.contains("help")) {
+                std::cerr
+                    << "Usage:\n"
+                       "  parsec <input-file> [<output-file>]\n"
+                       "  parsec [options]\n\n"
+                    << named_
+                    << '\n';
+            } else {
+                std::cerr << std::format(
+                    "parsec version {}: {}\n\n",
                     parsec::Config::version(),
                     parsec::Config::description()
-                ) << '\n' << '\n';
+                );
             }
+            return true;
+        }
+        notify(options_);
+        return false;
+    }
 
+
+    fs::path inputFile() const {
+        return options_["input-file"].as<fs::path>();
+    }
+
+    fs::path outputFile() const {
+        if(const auto& out = options_["output-file"]; !out.empty()) {
+            return out.as<fs::path>();
+        }
+        return inputFile().replace_extension(templateName());
+    }
+
+
+    fs::path templateDir() const {
+        if(const auto& tmplDir = options_["template-dir"]; !tmplDir.empty()) {
+            return tmplDir.as<fs::path>();
+        }
+
+        // use the directory placed along the executable as the source for templates
+        return fs::path(argv_[0]).remove_filename() / "templates" / "";
+    }
+
+    std::string templateName() const {
+        return options_["template"].as<std::string>();
+    }
+
+    fs::path templatePath() const {
+        return templateDir().append(templateName()).concat(".tmpl");
+    }
+
+
+private:
+    po::options_description named_;
+    po::variables_map options_;
+
+    int argc_ = {};
+    const char** argv_ = {};
+};
+
+
+class ParsecApp {
+public:
+
+    ParsecApp(const ParsecOptions* options)
+        : options_(options) {}
+
+    bool exec() {
+        input_.open(options_->inputFile(), std::ios::binary);
+        if(!input_.is_open()) {
+            throw std::runtime_error(std::format("failed to load the input file \"{}\"", options_->inputFile().string()));
+        }
+        input_.exceptions(std::ios::badbit | std::ios::failbit);
+        compiler_.setInputSource(&input_);
+
+        if(options_->templateName() != "json") {
+            tmpl_.open(options_->templatePath(), std::ios::binary);
+            if(!tmpl_.is_open()) {
+                throw std::runtime_error(std::format(
+                    "failed to load the template file \"{}\"",
+                    options_->templatePath().string()
+                ));
+            }
+            compiler_.setOutputTemplateSource(&tmpl_);
+        }
+
+        return compile();
+    }
+
+private:
+    bool compile() {
+        std::ostringstream compiled;
+        compiler_.setOutputSink(&compiled);
+        try {
+            compiler_.compile();
+        } catch(const parsec::ParseError& e) {
+            dumpError(e);
             return false;
         }
 
-        if(const auto it = options.find("template-path"); it != options.end()) {
-            tmplDir_ = it->second.as<fs::path>();
-        } else {
-            // use the directory placed along the executable as the source for templates
-            tmplDir_ = fs::path(argv[0]).remove_filename() / "templates" / "";
+        output_.open(options_->outputFile());
+        if(!output_.is_open()) {
+            throw std::runtime_error(std::format(
+                "failed to open the output file \"{}\"",
+                options_->outputFile().string()
+            ));
         }
-
-        if(const auto it = options.find("template"); it != options.end()) {
-            tmplName_ = it->second.as<std::string>();
-        }
-
-        inputPath_ = options["input-file"].as<fs::path>();
-        if(const auto it = options.find("output-file"); it != options.end()) {
-            outputPath_ = it->second.as<fs::path>();
-        } else {
-            outputPath_ = inputPath_;
-            outputPath_.replace_extension(tmplName_);
-        }
-
+        output_ << compiled.str();
         return true;
     }
 
-    void compileGrammar() {
-        if(input_.open(inputPath_, std::ios::binary); !input_.is_open()) {
-            throw std::runtime_error(std::format("failed to load the input file \"{}\"", inputPath_.generic_string()));
-        }
-        input_.exceptions(std::ios::badbit | std::ios::failbit);
-
-        // perform the compilation and save result in memory
-        parsec::Compiler compiler;
-        compiler.setInputSource(&input_);
-
-        if(!tmplName_.empty()) {
-            std::ifstream tmplFile;
-            if(tmplName_ != "json") {
-                const auto tmplPath = tmplDir_.append(tmplName_).concat(".tmpl");
-                if(tmplFile.open(tmplPath, std::ios::binary); !tmplFile.is_open()) {
-                    throw std::runtime_error(std::format("failed to load the template file \"{}\"", tmplPath.generic_string()));
-                }
-                compiler.setOutputTemplateSource(&tmplFile);
-            }
-
-            std::ostringstream outputMem;
-            compiler.setOutputSink(&outputMem);
-            compiler.compile();
-
-            std::ofstream outputFile(outputPath_);
-            if(!outputFile.is_open()) {
-                throw std::runtime_error(std::format("failed to load the output file \"{}\"", outputPath_.generic_string()));
-            }
-            outputFile << outputMem.str();
-        } else {
-            compiler.compile();
-        }
-    }
-
-
-    void dumpCurrentException() {
-        try {
-            throw;
-        } catch(const parsec::ParseError& e) {
-            printParseError(e);
-        } catch(const std::exception& e) {
-            std::cerr << "fatal error: " << e.what() << '\n';
-        } catch(...) {
-            std::cerr << "unknown error has occurred" << '\n';
-        }
-    }
-
-    void printParseError(const parsec::ParseError& err) {
-        const auto& errLoc = err.loc();
-        auto line = readInputLine(errLoc.line.offset);
-
-        const auto srcLineSize = line.size();
-        algo::trim_left(line);
-        const auto leadingWsSize = srcLineSize - line.size();
+    void dumpError(const parsec::ParseError& err) {
+        auto line = readInputLine(err.loc().line.offset);
         algo::trim_right(line);
 
-        const auto marker = makeVisualMarker(line, errLoc.startCol() - leadingWsSize, errLoc.colCount);
+        // remove leading whitespace
+        const auto srcLineSize = line.size();
+        algo::trim_left(line);
+        const auto markerPos = err.loc().startCol() - (srcLineSize - line.size());
+
+        std::size_t extraSpaces = 0;
+        std::string formatted;
+
+        for(std::size_t i = 0; i < line.size(); i++) {
+            // tabs are treated specially as they can cause the marker to be visually misplaced
+            if(line[i] == '\t') {
+                const auto tabDisplaySize = TabSize - (formatted.size() % TabSize);
+                formatted.append(tabDisplaySize, ' ');
+
+                if(i < markerPos) {
+                    extraSpaces += tabDisplaySize - 1;
+                }
+            } else {
+                formatted += line[i];
+            }
+        }
+
+        const auto spaces = std::string(markerPos + extraSpaces, ' ');
+        const auto marker = std::string(err.loc().colCount, err.loc().colCount == 1 ? '^' : '~');
         const auto indent = std::string(TabSize, ' ');
 
         std::cerr
-            << inputPath_.generic_string() << ':' << errLoc << ": error: " << err.what() << '\n'
-            << indent << line << '\n'
-            << indent << marker << '\n';
+            << options_->inputFile().string() << ':' << err.loc() << ": error: " << err.what() << '\n'
+            << indent << formatted << '\n'
+            << indent << spaces << marker << '\n';
     }
+
+    static constexpr auto TabSize = 4;
 
 
     std::string readInputLine(int linePos) {
@@ -189,32 +214,25 @@ private:
     }
 
 
-    static std::string makeVisualMarker(const std::string& line, std::size_t pos, std::size_t size) {
-        std::size_t spaces = 0;
-        for(const auto& ch : line | views::take(pos)) {
-            // tabs need to be treated specially as they can occur in the middle of a line
-            // and cause the visual marker to be placed in the wrong location
-            if(ch == '\t') {
-                // insert the required number of spaces to move to the next tab stop
-                const auto tabDisplaySize = TabSize - (spaces % TabSize);
-                spaces += tabDisplaySize;
-            } else {
-                spaces++;
-            }
-        }
-        return std::string(spaces, ' ') + std::string(size, size == 1 ? '^' : '~');
-    }
+    const ParsecOptions* options_ = {};
+    parsec::Compiler compiler_;
 
-    static constexpr int TabSize = 8;
-
-    fs::path inputPath_;
     std::ifstream input_;
-
-    fs::path tmplDir_;
-    std::string tmplName_;
-    fs::path outputPath_;
+    std::ifstream tmpl_;
+    std::ofstream output_;
 };
 
+
 int main(int argc, const char* argv[]) noexcept {
-    return ParsecApp().exec(argc, argv);
+    try {
+        auto options = ParsecOptions(argc, argv);
+        if(!options.dumpUsageInfo() && ParsecApp(&options).exec()) {
+            return 0;
+        }
+    } catch(const std::exception& e) {
+        std::cerr << "fatal error: " << e.what() << '\n';
+    } catch(...) {
+        std::cerr << "fatal error: unknown problem was encountered" << '\n';
+    }
+    return 1;
 }
