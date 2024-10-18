09085c456c10b66ff31879408a78dcb3c8283e22
cerfical <138404639+cerfical@users.noreply.github.com>
2024-10-18 22:07:57 +0500

    Refactor ELR automaton generation algorithm

--- src/core/CodeGen.cpp
+++ src/core/CodeGen.cpp
@@ -2,7 +2,7 @@
 #include "core/SymbolGrammar.hpp"
 
 #include "fsm/DfaStateGen.hpp"
-#include "fsm/ElrAutomaton.hpp"
+#include "fsm/ElrStateGen.hpp"
 #include "util/strings.hpp"
 
 #include <inja/inja.hpp>
@@ -51,51 +51,69 @@ namespace parsec {
         };
 
 
-        inja::json makeTransitions(std::span<const fsm::StateTrans> transitions) {
-            auto json = inja::json::array();
-            for(const auto& t : transitions) {
-                json.push_back({
-                    {  "label", strings::escape(t.label.text()) },
-                    { "target",                        t.target }
+        class GenerateJsonParseStates : private fsm::ElrStateGen::StateSink {
+        public:
+
+            inja::json run(const SymbolGrammar* rules) {
+                states_ = inja::json::array();
+
+                fsm::ElrStateGen()
+                    .setStateSink(this)
+                    .setInputGrammar(rules)
+                    .generate();
+
+                return std::move(states_);
+            }
+
+        private:
+            void addState(int id) override {
+                states_.push_back({
+                    {                "id",                  id },
+                    {    "is_start_state",             id == 0 },
+                    {    "is_match_state",               false },
+                    { "token_transitions", inja::json::array() },
+                    {  "rule_transitions", inja::json::array() },
+                    {         "backlinks", inja::json::array() },
+                    {             "match",                  "" }
                 });
             }
-            return json;
-        }
+
+            void addStateTokenTransition(int state, int target, const Symbol& label) override {
+                states_[state]["token_transitions"].push_back({
+                    {  "label", strings::escape(label.text()) },
+                    { "target",                        target }
+                });
+            }
+
+            void addStateRuleTransition(int state, int target, const Symbol& label) override {
+                states_[state]["rule_transitions"].push_back({
+                    {  "label", strings::escape(label.text()) },
+                    { "target",                        target }
+                });
+            }
+
+            void addStateBacklink(int state, int backlink) override {
+                states_[state]["backlinks"].push_back(backlink);
+            }
+
+            void setActiveBacklink(int state, int backlink) override {
+                states_[state]["active_backlink"] = backlink;
+            }
+
+            void setStateMatch(int state, const Symbol& match) override {
+                states_[state]["is_match_state"] = true;
+                states_[state]["match"] = match.text();
+            }
+
+            inja::json states_;
+        };
+
 
         inja::json generateJsonSymbols(const SymbolGrammar* grammar) {
             auto json = inja::json::array();
             if(grammar) {
                 for(const auto& s : grammar->symbols()) {
                     json.push_back(s.text());
-                }
-            }
-            return json;
-        }
-
-
-        inja::json makeBacklinks(std::span<const int> backlinks) {
-            auto json = inja::json::array();
-            for(const auto& backlink : backlinks) {
-                json.push_back(backlink);
-            }
-            return json;
-        }
-
-        inja::json makeParseStates(const SymbolGrammar* rules) {
-            auto json = inja::json::array();
-            if(rules) {
-                const auto elr = fsm::ElrAutomaton(*rules);
-                for(const auto& s : elr.states()) {
-                    json.push_back({
-                        {             "id",                       s.id() },
-                        { "is_start_state",             s.isStartState() },
-                        { "is_match_state",             s.isMatchState() },
-                        {         "shifts",  makeTransitions(s.shifts()) },
-                        {          "gotos",   makeTransitions(s.gotos()) },
-                        {      "backlinks", makeBacklinks(s.backlinks()) },
-                        {          "match",             s.match().text() },
-                        {       "backlink",                 s.backlink() }
-                    });
                 }
             }
             return json;
@@ -109,10 +127,10 @@ namespace parsec {
         }
 
         const inja::json vars = {
-            {      "token_names",         generateJsonSymbols(tokens_) },
-            {       "lex_states", GenerateJsonLexStates().run(tokens_) },
-            { "parse_rule_names",          generateJsonSymbols(rules_) },
-            {     "parse_states",              makeParseStates(rules_) }
+            {      "token_names",          generateJsonSymbols(tokens_) },
+            {       "lex_states",  GenerateJsonLexStates().run(tokens_) },
+            { "parse_rule_names",           generateJsonSymbols(rules_) },
+            {     "parse_states", GenerateJsonParseStates().run(rules_) }
         };
 
         if(tmpl_) {
---
+++ src/fsm/ElrStateGen.cpp
@@ -0,0 +1,231 @@
+#include "fsm/ElrStateGen.hpp"
+
+#include "core/NameConflictError.hpp"
+#include "core/SymbolGrammar.hpp"
+#include "fsm/DfaStateGen.hpp"
+
+#include <boost/functional/hash.hpp>
+
+#include <unordered_map>
+#include <unordered_set>
+
+namespace parsec::fsm {
+    namespace {
+        struct Item;
+    }
+}
+
+template <>
+struct boost::hash<parsec::fsm::Item> {
+    std::size_t operator()(const auto& item) const noexcept {
+        return boost::hash_value(std::tuple(item.dfaState, item.backlink));
+    }
+};
+
+namespace parsec::fsm {
+    namespace {
+        struct Item {
+
+            friend bool operator==(const Item& lhs, const Item& rhs) = default;
+
+            int dfaState = {};
+            int backlink = -1;
+        };
+
+        using ItemSet = std::unordered_set<Item, boost::hash<Item>>;
+
+
+        struct DfaStateTrans {
+            int target = {};
+            Symbol label;
+        };
+
+        struct DfaState {
+            std::vector<DfaStateTrans> transitions;
+            Symbol match;
+            int id = {};
+        };
+
+
+        class GenDfaStates : DfaStateGen::StateSink {
+        public:
+
+            GenDfaStates(std::vector<DfaState>* states, int baseStateId)
+                : states_(states), baseStateId_(baseStateId) {}
+
+            void run(const SymbolGrammar& grammar) {
+                DfaStateGen()
+                    .setInputGrammar(&grammar)
+                    .setStateSink(this)
+                    .generate();
+            }
+
+        private:
+            void addState(int id) override {
+                auto& s = states_->emplace_back();
+                s.id = id + baseStateId_;
+            }
+
+            void addStateTransition(int state, int target, const Symbol& label) override {
+                (*states_)[state + baseStateId_].transitions.emplace_back(target + baseStateId_, label);
+            }
+
+            void setStateMatch(int state, const Symbol& match) override {
+                (*states_)[state + baseStateId_].match = match;
+            }
+
+            std::vector<DfaState>* states_;
+            int baseStateId_ = {};
+        };
+
+
+        class TransNetwork {
+        public:
+
+            TransNetwork(const SymbolGrammar& grammar) {
+                for(const auto& symbol : grammar.symbols()) {
+                    if(const auto* const rule = grammar.resolve(symbol)) {
+                        const auto startStateId = static_cast<int>(states_.size());
+                        startStates_[symbol] = startStateId;
+
+                        GenDfaStates(&states_, startStateId)
+                            .run(SymbolGrammar().define(symbol, *rule));
+                    }
+                }
+            }
+
+
+            const DfaState* stateById(int state) const noexcept {
+                return &states_[state];
+            }
+
+            const DfaState* startState(const Symbol& symbol) const noexcept {
+                const auto symbolToIndexIt = startStates_.find(symbol);
+                if(symbolToIndexIt != startStates_.end()) {
+                    return &states_[symbolToIndexIt->second];
+                }
+                return nullptr;
+            }
+
+
+        private:
+            std::vector<DfaState> states_;
+            std::unordered_map<Symbol, int> startStates_;
+        };
+
+
+        class GenerateStates {
+        public:
+
+            GenerateStates(const SymbolGrammar& grammar, ElrStateGen::StateSink* sink)
+                : transNet_(grammar), grammar_(grammar), sink_(sink) {}
+
+            void run() {
+                if(const auto startState = createStartState(); !startState.empty()) {
+                    addState(startState);
+                }
+            }
+
+        private:
+            template <typename... Params, typename... Args>
+            void sink(void (ElrStateGen::StateSink::*func)(Params...), Args&&... args) {
+                if(sink_) {
+                    (sink_->*func)(std::forward<Args>(args)...);
+                }
+            }
+
+
+            ItemSet createStartState() {
+                ItemSet startState;
+                if(const auto* const root = grammar_.root()) {
+                    startState.insert({ .dfaState = transNet_.startState(*root)->id });
+                }
+                return startState;
+            }
+
+
+            int addState(const ItemSet& state) {
+                const auto [it, ok] = states_.try_emplace(closure(state), static_cast<int>(states_.size()));
+                const auto& [items, id] = *it;
+                if(ok) {
+                    sink(&ElrStateGen::StateSink::addState, id);
+                    for(const auto& item : items) {
+                        sink(&ElrStateGen::StateSink::addStateBacklink, id, item.backlink);
+                    }
+                    addStateTransitions(items, id);
+                }
+                return id;
+            }
+
+
+            ItemSet closure(const ItemSet& items) const {
+                ItemSet closure;
+                for(const auto& item : items) {
+                    closeItem(item, closure);
+                }
+                return closure;
+            }
+
+            void closeItem(const Item& item, ItemSet& closure) const {
+                if(closure.contains(item)) {
+                    return;
+                }
+                closure.insert(item);
+
+                for(const auto& trans : transNet_.stateById(item.dfaState)->transitions) {
+                    if(const auto* startState = transNet_.startState(trans.label)) {
+                        closeItem({ .dfaState = startState->id }, closure);
+                    }
+                }
+            }
+
+
+            void addStateTransitions(const ItemSet& items, int id) {
+                std::unordered_map<Symbol, ItemSet> transitions;
+                Symbol match;
+
+                for(int itemId = 0; const auto& item : items) {
+                    const auto& dfaState = transNet_.stateById(item.dfaState);
+                    if(dfaState->match) {
+                        if(!match) {
+                            sink(&ElrStateGen::StateSink::setStateMatch, id, dfaState->match);
+                            sink(&ElrStateGen::StateSink::setActiveBacklink, id, itemId);
+                            match = dfaState->match;
+                        } else {
+                            throw NameConflictError(match, dfaState->match);
+                        }
+                    }
+
+                    for(const auto& trans : dfaState->transitions) {
+                        transitions[trans.label].emplace(trans.target, itemId);
+                    }
+
+                    itemId++;
+                }
+
+                for(const auto& [label, transTarget] : transitions) {
+                    const auto targetId = addState(transTarget);
+                    const auto addTrans = grammar_.contains(label)
+                                            ? &ElrStateGen::StateSink::addStateRuleTransition
+                                            : &ElrStateGen::StateSink::addStateTokenTransition;
+                    sink(addTrans, id, targetId, label);
+                }
+            }
+
+
+            std::unordered_map<ItemSet, int, boost::hash<ItemSet>> states_;
+            TransNetwork transNet_;
+
+            const SymbolGrammar& grammar_;
+            ElrStateGen::StateSink* sink_ = {};
+        };
+    }
+
+
+    void ElrStateGen::generate() {
+        if(grammar_) {
+            GenerateStates(*grammar_, sink_)
+                .run();
+        }
+    }
+}
